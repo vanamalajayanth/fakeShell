3546008919fd8c48e6c39e6c7a1b1bf3233a51c5
VanamalaJayanth <183721726+vanamalajayanth@users.noreply.github.com>
2024-12-19 18:35:12 +0530

    added dome functionalities

--- nutShell.js
+++ nutShell.js
@@ -1,12 +1,69 @@
 const address = "vanamalajayanth@Vanamalas-MacBook-Pro";
+const directories = ['javaScript', 'shell', 'gitHub'];
+const files = [['hi.js', 'hello.js', 'goodMorning.js'],
+['shellPractice', 'shellSetUp', 'shellManual'], ['files', 'repsoitires']
+];
+
 let directory = [];
+
+const getLastDirIndex = function () {
+  return directories.indexOf(directory[directory.length - 1]);
+};
+
+const changePath = function (element) {
+  if (directory.length === 0 && directories.includes(element)) {
+    directory.push(element);
+    return;
+  }
+
+  const dirIndex = getLastDirIndex();
+
+  if (directory.length === 1 && files[dirIndex].includes(element)) {
+    directory.push(element);
+    return;
+  }
+
+  console.log(element, "no such file present");
+
+};
 
 const changeDir = function (args) {
   const array = args.split("/");
 
   for (const element of array) {
-    element === ".." ? directory.pop() : directory.push(element);
+    element === ".." ? directory.pop() : changePath(element);
   }
+};
+
+const getList = function () {
+  if (directory.length === 0) {
+    return directories.join('\n');
+  }
+  if (directory.length === 1) {
+    const dirIndex = getLastDirIndex();
+
+    return files[dirIndex].join('\n');
+  }
+
+  return "no files present";
+};
+
+const removeElemet = function (details) {
+  if (directory.length === 0) {
+    const index = directories.indexOf(details);
+    directories.splice(index, 1);
+    return;
+  }
+
+  if (directory.length === 1) {
+    const dirIndex = getLastDirIndex();
+    const eleIndex = files[dirIndex].indexOf(details);
+
+    files[dirIndex].splice(eleIndex, 1);
+    return;
+  }
+
+  return "no such file or directory present";
 };
 
 const getNextDir = function (commandArray) {
@@ -16,10 +73,22 @@ const getNextDir = function (commandArray) {
   switch (command) {
     case 'cd':
       changeDir(details);
+      return;
     case "pwd":
       console.log(directory.join("/"));
+      return;
+    case "ls":
+      console.log(getList());
+      return;
+    case 'echo':
+      console.log(details);
+      return;
+    case 'rm':
+      removeElemet(details);
+      return;
     default:
-      return "command not found";
+      console.log('command not present');
+      return;
   }
 };
 
